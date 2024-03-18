331ba728faf2ea5b7867caed6f3b47cee93aa45e
aleanse <aleanse_54@hotmail.com>
2024-03-18 11:46:46 -0300

    ksdak

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    text_post = models.TextField(max_length=200)
+    image = models.ImageField(upload_to='user/covers/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,related_name='posts')
