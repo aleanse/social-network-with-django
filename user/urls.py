5d92fe446fe11182953f3541d20024a6cc45cef6
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-03-24 01:59:15 -0300

    messages

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/',views.register, name="register"),
     path('create-register/', views.create_register, name="create-register"),
     path('login/', views.login_view, name="login"),
-    path('login/create-login/', views.create_login, name="create-login")
+    path('login/create-login/', views.create_login, name="create-login"),
+    path('logout/', views.logout_view, name='logout')
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from user.forms import RegisterForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import Http404
 from user.utils.authenticate import authenticate_by_email
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,9 +22,12 @@ def create_register(request):
     
 
 
+
 def login_view(request):
     form = LoginForm()
     return render(request,'login.html',context={'form':form})
+
+
 
 
 def create_login(request):
@@ -30,12 +35,24 @@ def create_login(request):
     if form.is_valid():
         user = authenticate_by_email(form.cleaned_data.get('email',''),
                                      form.cleaned_data.get('password'))
+
         if user is not None:
             login(request,user)
+            messages.success(request,'login feito com sucesso')
             return redirect('home')
+
         elif user is None:
-            return redirect('register')  
-        
+            messages.error(request,'Invalid credentials')
+
+    return redirect('login')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request,'logout feito com sucesso')
+    return redirect('home')
+
+
 def home(request):
     return render(request, 'home.html')
 
