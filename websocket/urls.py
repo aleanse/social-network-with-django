fc0540dc17e1da0c88325c72cd43a6080b423549
aleanse <aleanse_54@hotmail.com>
2024-04-11 12:12:34 -0300

    messages

--- websocket/urls.py
+++ websocket/urls.py
@@ -3,7 +3,7 @@ from websocket import views
 
 
 urlpatterns = [
-    path('messages/', views.CreateRoom, name='create-room'),
+    #path('messages/', views.CreateRoom, name='create-room'),
     path('<int:send_name>/<int:receiver_username>/', views.MessageView, name='room'),
     
 ]
--- websocket/views.py
+++ websocket/views.py
@@ -3,24 +3,27 @@ from user.models import User
 from .models import Message
 # Create your views here.
 
-def CreateRoom(request):
-    if request.method == 'POST':
-         user = request.user
-         sent_messages = Message.objects.filter(sender=user)
-         data = [{'sender': message.sender.username, 'receiver': message.receiver.username, 'content': message.content, 'timestamp': message.timestamp} for message in sent_messages]
-         return JsonResponse(data, safe=False)
-    return render(request, 'index.html')
+#def CreateRoom(request):
+ #   if request.method == 'POST':
+  #       user = request.user
+   #      sent_messages = Message.objects.filter(sender=user)
+    #     data = [{'sender': message.sender.username, 'receiver': message.receiver.username, 'content': message.content, 'timestamp': message.timestamp} for message in sent_messages]
+     #    return JsonResponse(data, safe=False)
+    #return render(request, 'index.html')
 
 
 def MessageView(request, send_name, receiver_username):
     sender = request.user
     receiver = User.objects.get(id=receiver_username)
     try:
-        message = message.objects.get(sender=sender,receiver=receiver)
+        message = Message.objects.get_or_create(sender=sender,receiver=receiver)
+        print(message)    
     except message.DoesNotExist:
-        message = message(sender=sender,receiver=receiver)
+        message = Message(sender=sender,receiver=receiver)
         message.save()    
-    return render(request, 'messages.html')
+    return render(request, 'messages.html',context={'messages':message,
+                                                    'sender':sender,
+                                                    'receiver':receiver})
 
 
 
