367d42fe70a41996d873611feaadfc08e113de2d
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-05-30 20:33:30 -0300

    refatorando-html-css

--- static/script.js
+++ static/script.js
@@ -1,8 +1,1 @@
 
-
-
-
-function toggleMenu() {
-    var navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active"); // Adiciona ou remove a classe 'active' para exibir ou esconder o menu
-}
