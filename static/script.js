410aaadfb2caa9b2baecb70d90557393fc1ec1ae
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-07-25 23:04:27 -0300

    sla

--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
 
+    document.addEventListener('DOMContentLoaded', () => {
+    const bell = document.querySelector('.notification-bell');
+    const sound = document.getElementById('notification-sound');
+    bell.addEventListener('click', () => {
+    if (bell.classList.contains('active')) {
+        bell.classList.remove('active');
+        sound.pause();
+        sound.currentTime = 0;
+    } else {
+        bell.classList.add('active');
+        sound.play();
+    }
+});
+});
+
+
+
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active"); // Adiciona ou remove a classe 'active' para exibir ou esconder o menu
+}
