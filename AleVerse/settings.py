6e52dff32ade722c160706c0ca84761bba93918d
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-07-25 23:33:50 -0300

    sda

--- AleVerse/settings.py
+++ AleVerse/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.static.StaticFilesMiddleware',
 
 
 ]
