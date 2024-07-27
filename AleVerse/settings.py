e2d3a6353c4e970ce085c11ca4f9e5f129a53caa
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-07-26 21:48:27 -0300

    tentando ws

--- AleVerse/settings.py
+++ AleVerse/settings.py
@@ -29,9 +29,9 @@ ENCRYPT_KEY = b'xLwj4srh_SzXdrDHAMwL4BLTkPo6V4savYP9xHX5YS0='
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
